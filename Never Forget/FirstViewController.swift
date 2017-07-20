a192abc2b75b41dd56d2184dae48260a491689ae
Jase-Omeileo West <omeileo@icloud.com>
2017-07-20 01:06:37 -0500

    Initial Commit

---
+++ Never Forget/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Never Forget
+//
+//  Created by Jase-Omeileo West on 7/20/17.
+//  Copyright © 2017 Omeileo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
